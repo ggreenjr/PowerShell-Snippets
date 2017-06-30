ebd4698d8fa188ed2036b361d0811a976cbd0566
Glen Green <ggreenjr@users.noreply.github.com>
2017-06-29 23:38:56 -0700

    Update Powershell-Verify_Admin.ps1
    Remove unnecessary formatting

--- Powershell-Verify_Admin.ps1
+++ Powershell-Verify_Admin.ps1
@@ -1,5 +1,4 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
-{
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
 Write-Warning "This script is not running this as an Administrator.  Please re-run this under an Administrator UAC context."
 Break 
 }
