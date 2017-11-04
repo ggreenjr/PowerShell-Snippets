170c8167d0366f9b1507b4c1af9d0a3e6e106f7d
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:34:07 -0700

    ExchPS Connect Server Update
    Merged the code for 'Connect Server' and 'Connect Server One' into the same snippet because the 'Connect Server One' code is more robust and can easily be modified to not check for existing Exchange Powershell connections if the user of the snippet desires.

--- Exchange-Powershell/PS-Code/ExchPowershell-Connect_Server.ps1
+++ Exchange-Powershell/PS-Code/ExchPowershell-Connect_Server.ps1
@@ -1,4 +1,9 @@
-﻿$exch_server "<Exchange FQDN>"
-$user_credential = (Get-Credential -Message "Exchange Administrative Credential")
-$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$exch_server/powershell/ -Credential $user_credential -Authentication Kerberos
+﻿$exch_server = "<Exchange FQDN>"
+$connection_state = (Get-PSSession | Where {$_.ConfigurationName -eq "Microsoft.Exchange"}).State
+
+if ($connection_state -ne "Opened") {
+$user_credential = Get-Credential -Message "Exchange Administrative Credential"
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://$exch_server/powershell/ -Credential $user_credential -Authentication Kerberos -AllowRedirection
 Import-PSSession $session
+Set-Variable -Name NewConnection -Value Yes
+}
