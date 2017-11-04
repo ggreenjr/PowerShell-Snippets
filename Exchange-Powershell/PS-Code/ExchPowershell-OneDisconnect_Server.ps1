0e003ef71cd17d9fabbeab918b9e2f1f8fb17aeb
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:48:26 -0700

    Remove OneDisconnect_Server
    Removed 'OneDisconnect_Server' snippet after it was merged into 'Disconnect_Server' snippet

--- Exchange-Powershell/PS-Code/ExchPowershell-OneDisconnect_Server.ps1
+++
@@ -1,4 +0,0 @@
-﻿Switch ($newconnection) {
-Yes {$sessions = (Get-PSSession | Where {$_.ConfigurationName -eq "Microsoft.Exchange"});$sessions | ForEach-Object {$sessionid = $_.id;Remove-PSSession $sessionid}}
-Default {}
-}
