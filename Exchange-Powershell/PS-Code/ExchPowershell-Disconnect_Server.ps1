f50564dfb5b1f46e6b9f53648aa49a89bd4130ac
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:46:13 -0700

    ExchPS Disconnect Server Update
    Updated 'Disconnect Server' code to include the 'One Disconnect Server' code because it is more robust and can easily be modified to not check for the 'NewConnection' variable.

--- Exchange-Powershell/PS-Code/ExchPowershell-Disconnect_Server.ps1
+++ Exchange-Powershell/PS-Code/ExchPowershell-Disconnect_Server.ps1
@@ -1,5 +1,4 @@
-﻿$sessions = (Get-PSSession | Where {$_.ConfigurationName -eq "Microsoft.Exchange"})
-$sessions | ForEach-Object {
-$sessionid = $_.id
-Remove-PSSession $sessionid
+﻿Switch ($newconnection) {
+Yes {$sessions = (Get-PSSession | Where {$_.ConfigurationName -eq "Microsoft.Exchange"});$sessions | ForEach-Object {$sessionid = $_.id;Remove-PSSession $sessionid}}
+Default {}
 }
