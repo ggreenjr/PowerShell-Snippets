32240d0d9c48878873d441365ef1d9ba3a000559
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 09:39:03 -0700

    Export_Loop Update
    Updated export loop to catch on any terminating error, not just one specific one.

--- Powershell/PS-Code/Powershell-Export_Loop.ps1
+++ Powershell/PS-Code/Powershell-Export_Loop.ps1
@@ -5,7 +5,7 @@
 Do {
 [string]$error_status = "NoError"
 Try {$filewrite | Export-Csv -Path $outfile -NoTypeInformation}
-Catch [System.IO.IOException] {$error_status="Error";Write-Warning "Failed Write $failed_write_attempt";Start-Sleep -Seconds 5}
+Catch {$error_status="Error";Write-Warning "Failed Write $failed_write_attempt";Start-Sleep -Seconds 5}
 $failed_write_attempt++
 }While ($error_status -eq "Error")
 
