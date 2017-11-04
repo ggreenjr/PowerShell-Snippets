5473eb0d4a0770d07fa30b90b7175835c3115f4b
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:23:21 -0700

    Labeled Loop Update

--- Powershell/PS-Code/Powershell-Loop_Label.ps1
+++ Powershell/PS-Code/Powershell-Loop_Label.ps1
@@ -1,1 +1,5 @@
-﻿:$loopname ForEach ($server in $server_list) {$scriptblock}
+﻿$server_list = "server1.example.com","server2.example.com","server3.example.com","server4.example.com","server5.example.com"
+
+:LoopName ForEach ($server in $server_list) {
+Insert action code here
+}
