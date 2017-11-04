cf22585091e14fb24147b834827cb06ed5073ba0
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:17:39 -0700

    Verify Admin Update
    Updated previous code which was unnecessary and reduced it to a single 'Requires' statement which gets placed at the top of the script

--- Powershell/PS-Code/Powershell-Verify_Admin.ps1
+++ Powershell/PS-Code/Powershell-Verify_Admin.ps1
@@ -1,4 +1,1 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-Write-Warning "This script is not running this as an Administrator.  Please re-run this under an Administrator UAC context."
-Break 
-}
+#Requires -RunAsAdministrator
