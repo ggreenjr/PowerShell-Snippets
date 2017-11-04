926812dad83f06c0930ab3dd1591f6b1c1bbeebb
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 08:07:04 -0700

    Export_Loop Snippet Update
    Changed variable names to be easier to understand their function
    Specified variable types
    Changed output to write to the warning pipeline instead of the host
    Removed extraneous scriptblock brackets that cause problems when the snippet is used as was written

--- Powershell/PS-Code/Powershell-Export_Loop.ps1
+++ Powershell/PS-Code/Powershell-Export_Loop.ps1
@@ -1,11 +1,11 @@
-﻿$filewrite = "This is a file write test"
+﻿[string]$outfile = "Output:\outfile.csv"
+[string]$filewrite = "This is a file write test"
+[int]$failed_write_attempt = 1
 
-{
-$i = 1
 Do {
-$a = "NoError"
+[string]$error_status = "NoError"
 Try {$filewrite | Export-Csv -Path $outfile -NoTypeInformation}
-Catch {[System.IO.IOException] | Out-Null ;$a="Error";Write-Host "Failed Write $i";Start-Sleep -Seconds 5}
-$i++
-}While ($a -eq "Error")
-}
+Catch [System.IO.IOException] {$error_status="Error";Write-Warning "Failed Write $failed_write_attempt";Start-Sleep -Seconds 5}
+$failed_write_attempt++
+}While ($error_status -eq "Error")
+
