969f3735a11d57c009121a8d0e435b9c1a551f7b
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 10:21:35 -0700

    Export Loop Update
    Added tabs where appropriate to allow for easier reading of nested loops and other code.

--- Powershell/PS-Code/Powershell-Export_Loop.ps1
+++ Powershell/PS-Code/Powershell-Export_Loop.ps1
@@ -1,11 +1,9 @@
 ﻿[string]$outfile = "Output:\outfile.csv"
 [string]$filewrite = "This is a file write test"
 [int]$failed_write_attempt = 1
-
 Do {
-[string]$error_status = "NoError"
-Try {$filewrite | Export-Csv -Path $outfile -NoTypeInformation}
-Catch {$error_status="Error";Write-Warning "Failed Write $failed_write_attempt";Start-Sleep -Seconds 5}
-$failed_write_attempt++
+    [string]$error_status = "NoError"
+    Try {$filewrite | Export-Csv -Path $outfile -NoTypeInformation}
+    Catch {$error_status="Error";Write-Warning "Failed Write $failed_write_attempt";Start-Sleep -Seconds 5}
+    $failed_write_attempt++
 }While ($error_status -eq "Error")
-
