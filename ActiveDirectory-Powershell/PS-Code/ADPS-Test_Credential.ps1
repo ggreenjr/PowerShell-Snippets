0cf71a05b10872c058a3a3cfe1b94b52c4121df6
Glen Green <ggreenjr@users.noreply.github.com>
2017-11-04 18:35:00 -0700

    Snippet File Formatting Update

---
+++ ActiveDirectory-Powershell/PS-Code/ADPS-Test_Credential.ps1
@@ -0,0 +1,13 @@
+﻿function Test-Credential {
+$credential = (Get-Credential -Message "AD credentials to test")
+$username = $credential.username
+$password = $credential.GetNetworkCredential().Password
+$auth_check = (New-Object directoryservices.directoryentry "",$username,$password).psbase.name -ne $null
+
+Switch ($auth_check) {
+True {Write-Host "Active Directory authentication check succeeded." -Foregroundcolor Green}
+False {Write-Host "Active Directory authentication check failed." -Foregroundcolor Red}
+Default {Write-Host "Unknown result from the authentication check." -Foregroundcolor Yellow}
+}
+}
+
